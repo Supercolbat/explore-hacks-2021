6ccc5d6999b98a9e97b477fff41b73b74e19b15e
Kevin G <ge.kevin.a@gmail.com>
2021-07-23 21:38:56 -0500

    Changed purple50

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,7 +4,7 @@ class ColorPalette {
   static const Color purple200 = Color(0xFF8B35C0);
   static const Color purple150 = Color(0xFFA742E4);
   static const Color purple100 = Color(0xFFB67ADB);
-  static const Color purple50 = Color(0xFFF9E0FF);
+  static const Color purple50 = Color(0xFFFAE4FF);
 
   static const Color grey150 = Color(0xFF5F5F5F);
   static const Color grey100 = Color(0xFF767676);
