7c66a9f0d7f01fb6ec6907e355ce7b6308a9b631
Kevin G <ge.kevin.a@gmail.com>
2021-07-25 01:03:35 -0500

    Added search by tags

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,98 +1,125 @@
+import 'package:async/async.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:explore_hacks_2021/models/opportunity.dart';
 import 'package:explore_hacks_2021/screens/home/volunteer_list.dart';
 import 'package:flutter/material.dart';
 import 'package:explore_hacks_2021/constants/colors.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:line_icons/line_icons.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    var opp = FirebaseFirestore.instance.collection('opportunities');
-    return FutureBuilder(
-      future: opp.get(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-            snapshot.hasData) {
-          Size size = MediaQuery.of(context).size;
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-          List<Opportunity> oppList = (snapshot.data! as QuerySnapshot)
-              .docs
-              .map((QueryDocumentSnapshot d) => new Opportunity(
-                    id: d.id,
-                    address: d.get("address").toString(),
-                    name: d.get("name").toString(),
-                    availableSpots:
-                        int.parse(d.get("availableSpots").toString()),
-                    description: d.get("description").toString(),
-                    date: d.get("date").toDate(),
-                  ))
-              .toList();
+class _HomeScreenState extends State<HomeScreen> {
+  String tagQuery = '';
+  List<Opportunity> oppListState = [];
+  final AsyncMemoizer _memoizer = AsyncMemoizer();
 
-          return Scaffold(
-            body: Container(
-              width: size.width,
-              color: ColorPalette.purple50,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: size.width * 0.12,
-                    top: size.height * 0.09,
-                    child: Text(
-                      "Volunteering",
-                      style: TextStyle(
-                        fontFamily: 'Geometria',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 23,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    child: VolunteerList(
-                      oppList: oppList,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-        return CircularProgressIndicator();
-      },
-    );
+  _fetchData() {
+    return _memoizer.runOnce(() async {
+      var snapshot =
+          await FirebaseFirestore.instance.collection('opportunities').get();
+      return snapshot.docs
+          .map((QueryDocumentSnapshot d) => new Opportunity(
+                id: d.id,
+                address: d.get("address").toString(),
+                name: d.get("name").toString(),
+                availableSpots: int.parse(d.get("availableSpots").toString()),
+                description: d.get("description").toString(),
+                date: d.get("date").toDate(),
+              ))
+          .toList();
+    });
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   Size size = MediaQuery.of(context).size;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
 
-  //   return Scaffold(
-  //     body: Container(
-  //       width: size.width,
-  //       color: ColorPalette.purple50,
-  //       child: Stack(
-  //         children: [
-  //           Positioned(
-  //             left: size.width * 0.12,
-  //             top: size.height * 0.09,
-  //             child: Text(
-  //               "Volunteering",
-  //               style: TextStyle(
-  //                 fontFamily: 'Geometria',
-  //                 fontWeight: FontWeight.w500,
-  //                 fontSize: 23,
-  //               ),
-  //             ),
-  //           ),
-  //           Positioned(
-  //             bottom: 0,
-  //             child: VolunteerList(),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: size.width,
+        color: ColorPalette.purple50,
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 30, top: 40),
+              child: Text(
+                "Volunteering",
+                style: TextStyle(
+                  fontFamily: 'Geometria',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 23,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Container(
+              height: 55,
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                onChanged: (e) {
+                  setState(() {
+                    this.tagQuery = e;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Search by tags',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: ColorPalette.purple200, width: 1),
+                  ),
+                  suffixIcon: Icon(
+                    LineIcons.search,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('opportunities')
+                    .where('type', isEqualTo: tagQuery)
+                    .get()
+                    .then((snapshot) {
+                  return snapshot.docs
+                      .map((QueryDocumentSnapshot d) => new Opportunity(
+                            id: d.id,
+                            address: d.get("address").toString(),
+                            name: d.get("name").toString(),
+                            availableSpots:
+                                int.parse(d.get("availableSpots").toString()),
+                            description: d.get("description").toString(),
+                            date: d.get("date").toDate(),
+                          ))
+                      .toList();
+                }),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    List<Opportunity> oppList =
+                        snapshot.data as List<Opportunity>;
+
+                    return VolunteerList(
+                      oppList: oppList,
+                    );
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/screens/home/volunteer_list.dart
+++ lib/screens/home/volunteer_list.dart
@@ -1,8 +1,6 @@
 import 'package:explore_hacks_2021/constants/colors.dart';
 import 'package:explore_hacks_2021/models/opportunity.dart';
 import 'package:explore_hacks_2021/screens/home/volunteer_item.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -16,13 +14,50 @@ class VolunteerList extends StatefulWidget {
 }
 
 class _VolunteerListState extends State<VolunteerList> {
-  int _slideIndx = 1;
+  int _slideIndx = 0;
 
   final PanelController _panelController = PanelController();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
+    if (widget.oppList.length < 1) {
+      return Container(
+        width: size.width,
+        height: size.height * 0.68,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Color(0xFFF9E0FF), offset: Offset(0, 0), blurRadius: 10)
+          ],
+        ),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 37, left: size.width * 0.11),
+                  child: Text(
+                    "Opportunities near you",
+                    style: TextStyle(
+                      fontFamily: 'Geometria',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
     return Container(
       width: size.width,
       height: size.height * 0.68,
@@ -119,99 +154,4 @@ class _VolunteerListState extends State<VolunteerList> {
       ),
     );
   }
-  // @override
-  // Widget build(BuildContext context) {
-  //   var opp = FirebaseFirestore.instance.collection('opportunities');
-  //   return FutureBuilder(
-  //       future: opp.get(),
-  //       builder: (context, snapshot) {
-  //         if (snapshot.connectionState == ConnectionState.done &&
-  //             snapshot.hasData) {
-  //           Size size = MediaQuery.of(context).size;
-
-  //           List<Opportunity> oppList = (snapshot.data! as QuerySnapshot)
-  //               .docs
-  //               .map((QueryDocumentSnapshot d) => new Opportunity(
-  //                     id: d.id,
-  //                     address: d.get("address").toString(),
-  //                     name: d.get("name").toString(),
-  //                     availableSpots:
-  //                         int.parse(d.get("availableSpots").toString()),
-  //                     description: d.get("description").toString(),
-  //                     date: d.get("date").toDate(),
-  //                   ))
-  //               .toList();
-  //           /*
-  //             new Opportunity(
-  //                 address: '1600 Pennsylvania Ave NW, Washington, DC',
-  //                 avaiableSpots: 25,
-  //                 date: DateTime(2021, 10, 14),
-  //                 description: 'Let\'s all clean dogs together',
-  //                 name: 'Dog grooming convention',
-  //                 organization: '123'),
-  //             new Opportunity(
-  //                 address: '8845 Long Road',
-  //                 avaiableSpots: 157,
-  //                 date: DateTime(2021, 7, 29),
-  //                 description: 'Help make meals for schools',
-  //                 name: 'Houston Food Bank',
-  //                 organization: '123'),
-  //             new Opportunity(
-  //                 address: '4458 Yellow Brick Ln.',
-  //                 avaiableSpots: 5,
-  //                 date: DateTime(2021, 8, 7),
-  //                 description: 'Pick up trash',
-  //                 name: 'Clean up the beach',
-  //                 organization: '123')
-  //           ];*/
-  //           return Container(
-  //             width: size.width,
-  //             height: size.height * 0.68,
-  //             decoration: BoxDecoration(
-  //               color: Colors.white,
-  //               borderRadius: BorderRadius.only(
-  //                 topLeft: Radius.circular(40),
-  //                 topRight: Radius.circular(40),
-  //               ),
-  //               boxShadow: [
-  //                 BoxShadow(
-  //                     color: Color(0xFFF9E0FF),
-  //                     offset: Offset(0, 0),
-  //                     blurRadius: 10)
-  //               ],
-  //             ),
-  //             child: Column(
-  //               crossAxisAlignment: CrossAxisAlignment.start,
-  //               children: [
-  //                 Container(
-  //                   margin: EdgeInsets.only(top: 37, left: size.width * 0.11),
-  //                   child: Text(
-  //                     "Opportunities near you",
-  //                     style: TextStyle(
-  //                       fontFamily: 'Geometria',
-  //                       fontWeight: FontWeight.w400,
-  //                       fontSize: 17,
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 Container(
-  //                   alignment: Alignment.center,
-  //                   margin: EdgeInsets.only(
-  //                     top: 37,
-  //                   ),
-  //                   child: Column(
-  //                     children:
-  //                         oppList.map((opp) => VolunteerItem(opp)).toList(),
-  //                   ),
-  //                 )
-  //               ],
-  //             ),
-  //           );
-  //         }
-  //         return Center(
-  //             child: CircularProgressIndicator(
-  //           valueColor: AlwaysStoppedAnimation<Color>(ColorPalette.purple150),
-  //         ));
-  //       });
-  // }
 }
